40a0f48e962e251b12710ddb9cabb36cce580576
Durbaca <dianadurbaca2003@gmail.com>
2025-08-06 13:42:57 +0300

    feat: enhance planet details display and improve project links structure

--- src/three-custom.d.ts
+++ src/three-custom.d.ts
@@ -1,16 +1,13 @@
-// three-custom.d.ts
 import * as THREE from 'three';
 
 declare module 'three' {
   interface WebGLRenderer {
-    // proprietăți pentru toneMapping + gamma
     toneMapping: number;
     outputEncoding: number;
     gammaOutput: boolean;
     gammaFactor: number;
   }
   interface Texture {
-    // proprietate pentru encoding textură
     encoding: number;
   }
 }
