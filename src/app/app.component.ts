48de3a45bcffaae424b73ac268978e273b9a6977
Durbaca <dianadurbaca2003@gmail.com>
2025-08-12 19:27:42 +0300

    Add Google Analytics tracking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,20 @@ import { RouterModule } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+
+  private router = inject(Router);
+
+  constructor() {
+    this.router.events
+      .pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd))
+      .subscribe((event) => {
+        (window as any).gtag?.('event', 'page_view', {
+          page_title: document.title,
+          page_path: event.urlAfterRedirects,
+          page_location: location.href,
+        });
+      });
+  }
+
+}
